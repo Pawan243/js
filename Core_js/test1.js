761d79674c325e89986685a031f0ad7aad48b128
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-10-20 11:50:33 +0530

    added test1

---
+++ Core_js/test1.js
@@ -0,0 +1,14 @@
+console.time('Your Code Took')
+console.log("Hello Pawan");
+console.log(2+5);
+console.log(6);
+console.log(true);
+console.log([3,5,2,8]);
+console.log({pawan:'hello', swami:'there'});
+console.table({pawan:'hello', swami:'there'});
+console.warn('This is a warning');
+console.timeEnd('Your Code Took')
+console.error('This is an error')
+console.assert(488<188, 'This is not possible')
+console.assert(550<100, 'testing');
+console.error('This is an error');
