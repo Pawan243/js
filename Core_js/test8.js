da8c7ecc47becb3e341a53a2925b6e86e1cd4fe6
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-10-20 11:53:54 +0530

    added test8

---
+++ Core_js/test8.js
@@ -0,0 +1,11 @@
+console.log('This is 8th test in the series of javascript learning.')
+
+const age = 19;
+
+if ( age == 19){
+    console.log('age is 19')
+}
+
+else{
+    console.log('age is not 19')
+}
