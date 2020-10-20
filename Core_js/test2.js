edc8e7d0f00acf86e48584be367f3d71a7ea2f8f
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-10-20 11:50:53 +0530

    added test2

---
+++ Core_js/test2.js
@@ -0,0 +1,44 @@
+console.log('welcome tut3');
+// variables in js
+// var, let, const
+
+var name = 'pawan'
+console.log(name)
+var channel;
+var marks =34+ 23;
+// channel = 'codewithpawan'
+console.log(name, channel,marks)
+
+//Rules for creating Javascript variable
+/*
+1. Cannot start with numbers
+2. Can start with letter , numbers, - or $
+3. Are case sensitive
+*/
+
+var city = 'kolkata';
+console.log(city);
+
+const name1 = 'Hari Ram';
+console.log(name1)
+
+// name1 = 'pawan'(cannot do this because constant variable takes values once.)
+
+//  const variable must be intialise with some values.
+// const fruit; (cannot write this)
+
+
+{
+    let city = 'Rampur';
+    city = 'mumbai'
+    console.log(city)
+}
+console.log(city)
+
+const arr1 = [1,4,6,9,3]
+arr1.push(23)
+console.log(arr1)
+/* 
+camelcase
+kababcase
+*/
