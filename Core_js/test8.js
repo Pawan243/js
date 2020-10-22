032b86801bf44dacfe23b23d602e76f80a1772df
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-10-22 15:59:19 +0530

    modified

--- Core_js/test8.js
+++ Core_js/test8.js
@@ -1,11 +1,31 @@
 console.log('This is 8th test in the series of javascript learning.')
 
-const age = 19;
+// const age = 19;
 
-if ( age == 19){
-    console.log('age is 19')
-}
+// if ( age == 19){
+//     console.log('age is 19')
+// }
 
-else{
-    console.log('age is not 19')
+// else {
+//     console.log('age is not 19')
+// }
+
+
+console.log(age = 19 ? 'Age is 19' : 'Age is not 19')
+
+switch (age) {
+    case 32:
+        console.log('age is not 32')
+        break;
+
+    case 25:
+        console.log('age is not 25')
+        break;
+    case 89:
+        console.log('age is not 89')
+        break;
+
+    default:
+        console.log('your age is unknown')
+        break;
 }
