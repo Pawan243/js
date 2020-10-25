d24d99588ce278a9512c06c9a7ccaff98b7672fe
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-10-25 13:21:54 +0530

    Manipulating Websites Using JS Window Object

---
+++ Core_js/test11.js
@@ -0,0 +1,18 @@
+console.log('Welcome to my 11th test script in the series of javascript learning.')
+
+let a = window;
+alert('hello pawan')
+
+a = prompt('this will destroy your computer. type your name')
+
+a = confirm('Are you sure that you want to delete this page?')
+
+let a = window.document;
+
+a = window.innerHeight;
+a = innerWidth; 
+a = scrollY;
+a = scrollX;
+a = location.toString();
+a = window.history;
+console.log(a)
