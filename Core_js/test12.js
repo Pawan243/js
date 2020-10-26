f78eeba0c0a70675c932339b823ccb9502c4e27c
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-10-26 18:48:06 +0530

    javascript DOM concept

---
+++ Core_js/test12.js
@@ -0,0 +1,18 @@
+console.log('Welcome to my 12th test script in the series of javascript learning.')
+
+let a = document;
+a = document.all
+// a = document.body;
+// a = document.forms[0] 
+
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// })
+a = document.links[0];
+a = document.links[0].href;
+
+var x = document.images;
+a = document.scripts;
+// document.images and document.scripts and print the list of images and scripts on an html pages
+
+console.log(a)
