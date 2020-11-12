2d61d044056f34b89f346318f112ca4cb9288a1f
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-11-12 08:25:19 +0530

    creating an editable div using javascript

--- Core_js/test21.js
+++ Core_js/test21.js
@@ -4,3 +4,34 @@ You have to create a div and inject it into the page which contains a heading.
 whenever someone clicks on the div, it should be converted into an editable item. whenever user clicks away (blur). save the note into the local storage.
 
 */
+
+let area = null;
+let view = document.getElementById('view');
+
+view.onclick = function () {
+    editStart();
+};
+
+function editStart() {
+    area = document.createElement('textarea');
+    area.className = 'edit';
+    area.value = view.innerHTML;
+
+    area.onkeydown = function (event) {
+        if (event.key == 'Enter') {
+            this.blur();
+        }
+    };
+
+    area.onblur = function () {
+        editEnd();
+    };
+
+    view.replaceWith(area);
+    area.focus();
+}
+
+function editEnd() {
+    view.innerHTML = area.value;
+    area.replaceWith(view);
+}
