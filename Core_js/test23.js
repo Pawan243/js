2293e709686a1a72935b4345bf9569f4adb28dad
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2021-03-07 10:38:30 +0530

    update test23.js

---
+++ Core_js/test23.js
@@ -0,0 +1,26 @@
+console.log('welcome to test23.js in the series of js learning')
+
+let x = 3;
+let y = 4;
+
+let z;
+z = x+y;
+z = x-y;
+z = x*y;
+z = x/y;
+// exploring the math object
+z = Math.E
+z = Math.PI
+z = Math.round(5.4)
+z = Math.ceil(5.3)
+z = Math.floor(5.3)
+z = Math.abs(-57.2)
+z = Math.sqrt(49)
+z = Math.pow(2,4)
+z = Math.min(3,5,2,6)
+z = Math.max(4,67,23,89)
+z = Math.random();
+z = 100*Math.random();
+a = (0,1)
+a100 = (0,100);
+console.log(z);
