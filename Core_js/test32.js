8f06b099653250bb9bb2a5c8cf515b640d5fceb4
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2021-03-15 18:34:08 +0530

    Exercise for Implementing a library class

---
+++ Core_js/test32.js
@@ -0,0 +1,6 @@
+console.log('Welcome to test32.js in the series of javascript')
+//constructor must take the book list as an argument
+// create a class library and implement the following:
+// getBookList()
+// issueBook(bookname, user)
+// returnBook(bookname)
